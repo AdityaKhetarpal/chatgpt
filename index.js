78b2806c7957bb72611d79d7cd3eb838c69092a0
Aditya Khetarpal <80028073+AdityaKhetarpal@users.noreply.github.com>
2023-02-04 19:21:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const checkForKey = () => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(['openai-key'], (result) => {
+        resolve(result['openai-key']);
+      });
+    });
+  };
+
+const encode = (input) => {
+    return btoa(input);
+};
+const saveKey = () => {
+    const input = document.getElementById('key_input');
+if(input){
+    const  {value} = input;
+   //Encode String
+    const encodedValue = encode(value);
+
+    const changeKey = () => {
+    document.getElementById('key_needed').style.display = 'block';
+    document.getElementById('key_entered').style.display = 'none';
+    };
+}
+};
+
+checkForKey().then((response) => {
+    if(response) {
+        document.getElementById('key_needed').style.display = 'none'
+        document.getElementById('key_entered').style.display = 'block';
+    }
+});
+
+
